e8ff6cf727598a126287845212fde4f8a0b1a7ea
bogdanluca14 <bogdanlucaalexandru@gmail.com>
2025-07-12 19:01:32 +0300

    Update app.py

--- app.py
+++ app.py
@@ -782,7 +782,7 @@ if submit:
             options=top5,
             index=0,
             help="Alege unul dintre primele 5 joburi recomandate",
-            label_visibility='collapsed
+            label_visibility='collapsed'
         )
 
         # Afișăm expandere pentru fiecare din top5, dar în layout vertical
